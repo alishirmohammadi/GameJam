9d0d801554631df518d4135c233f443669db0b8e
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-01-26 13:05:35 +0330

    first commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public GameObject[] Routes;
+    public float Speed, RotationSpeed;
+    public float TargetChangeDistance;
+
+
+    private int TargetIndex;
+    private Transform[] RoutePoints;
+    
+    void Start()
+    {
+        GameObject Route = Routes[Random.Range(0, Routes.Length)];
+        RoutePoints = Route.GetComponentsInChildren<Transform>();
+        transform.position = RoutePoints[1].position;
+        TargetIndex = 2;
+    }
+
+    void Update() {
+        Vector3 direction = RoutePoints[TargetIndex].position - transform.position;
+        Quaternion toRotation = Quaternion.LookRotation(direction, transform.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, toRotation, RotationSpeed * Time.time);
+//        transform.LookAt(RoutePoints[TargetIndex]);
+        transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        float distance = (RoutePoints[TargetIndex].position - transform.position).magnitude;
+        if (distance < TargetChangeDistance)
+        {
+            if(TargetIndex < RoutePoints.Length - 1)
+                TargetIndex++;
+            else
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Human/Mommy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mommy : MonoBehaviour
+{
+    public float DamageRadious;
+    public float DamageTimeDistance;
+    public Transform Target;
+
+    private float LastDamageTime = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!Target)
+        {
+            Collider[] colliders;
+            colliders = Physics.OverlapSphere(transform.position, DamageRadious);
+            if (colliders.Length > 0)
+                Target = colliders[0].gameObject.transform;
+        }
+        else
+        {
+//            GameObject gun = (GameObject) Instantiate()
+        }
+    }
+}
